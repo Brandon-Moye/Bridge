4db76f0458480ee139b99e3b7d52a5cd1ef44211
Brandon Moye <85130587+Brandon-Moye@users.noreply.github.com>
2024-03-26 10:41:00 -0500

    setting up signup header

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom"
 
 export default function Signup() {
   return <div>Signup</div>;

3be02320928b3c2232b17f4ae3f6f583a2ad25eb
Brandon Moye <brandon.m.moye@gmail.com>
2024-04-02 20:21:42 -0500

    updates

--- src/components/BlogArray.jsx
+++ src/components/BlogArray.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-
+import React from "react";
 
 const BlogArray = [
-    {
-        postNum: 2,
-        title: "Post #2 First Feature",
-        date: "March 24, 2024",
-        postType: "Project Documenting",
-        textBody: "The first feature I will develop is to allow users to track and store daily gratitudes. I want to have their gratitudes saved in MongoDB and have them access their history. After that, I will develop users to friend each other and see each other's gratitudes."
-    },
-    {
-        postNum: 1,
-        title: "Post #1 Introductions",
-        date: "March 23, 2024",
-        postType: "Project Documenting",
-        textBody: "Hello, this is the first blog post for this app. I am seeking to build an app that can genuinely connect people without destroying their focus, lives, and mental health. If you are seeing this, this is the first day code was put into this site, it is very new and raw. Also, how did you find this? It is not publicized anywhere. That is kind of weird of you. Thank you for visiting"
-    }
-]
+  {
+    postNum: 3,
+    title: "Post #3 Getting Comfy with Mongo",
+    date: "April 2, 2024",
+    postType: "Project Documenting",
+    textBody:
+      "I have 3 working functions with MongDB and the CRU in my CRUD on the horizon! MongoDB is an amazing tool and if you're reading this, love to code, and don't know how to use it, GO LEARN IT! I am excited for the first revision of this application to be complete. That will incompass allowing users to log in and write gratitudes for the day, they can see old gratitudes, update old ones, or delete them if they feel inclined. I have learned so much and I am still in phase one. I am very excited to continue working on this project. PS - yes the pages are UGLY but styilng will be coming soon!",
+  },
+  {
+    postNum: 2,
+    title: "Post #2 First Feature",
+    date: "March 24, 2024",
+    postType: "Project Documenting",
+    textBody:
+      "The first feature I will develop is to allow users to track and store daily gratitudes. I want to have their gratitudes saved in MongoDB and have them access their history. After that, I will develop users to friend each other and see each other's gratitudes.",
+  },
+  {
+    postNum: 1,
+    title: "Post #1 Introductions",
+    date: "March 23, 2024",
+    postType: "Project Documenting",
+    textBody:
+      "Hello, this is the first blog post for this app. I am seeking to build an app that can genuinely connect people without destroying their focus, lives, and mental health. If you are seeing this, this is the first day code was put into this site, it is very new and raw. Also, how did you find this? It is not publicized anywhere. That is kind of weird of you. Thank you for visiting",
+  },
+];
 
 export default BlogArray;
-

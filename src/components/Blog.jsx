3c6f917bdd730e2bd86c65cc12751d5336f18473
Brandon Moye <85130587+Brandon-Moye@users.noreply.github.com>
2024-04-21 16:20:43 -0500

    starting some styling

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 const Blog = ({ post }) => {
     return (
-        <div className="blogPost grid grid-cols-2 auto-rows-min p-2 p-0.5">
+        <div className="blogPostWrapper grid grid-cols-2 auto-rows-min p-2 p-0.5">
             <h2 className="text-xl font-semibold sm:text-3xl col-span-2 postTitle bg-mainColor">{post.title}</h2>
             <h3 className="row-start-2">{post.date}</h3>
             <p className="row-start-2 mb-6">{post.postType}</p>
